a10041296c7231177c9db97795a79ee0f6c48183
Abdullah <abdullahdeveloper843@gmail.com>
2026-01-12 10:15:10 +0500

    Create bare bone structure for the central data flow of communication between AI and Human.

--- redux/slices/chatSlice.js
+++ redux/slices/chatSlice.js
@@ -1,16 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  messages: [],
+  query: "",
+  response: "",
+  currentExchange: { query: "", response: "" },
+  chatHistory: [],
 };
 
 const chatSlice = createSlice({
   name: "chat",
   initialState,
   reducers: {
-    sendMessage: (state, action) => {},
+    sendResponse: (state, action) => {
+      state.response = action.payload.reFsponse;
+      state.currentExchange.response = action.payload.response;
+    },
+    storeQuery: (state, action) => {
+      state.query = action.payload.query;
+      state.currentExchange.query = action.payload.query;
+    },
+    addToChatHistory: (state) => {
+      // store the current exchange in chat history. Spread operator is used to create a new object to avoid reference issues.
+      // If we just pushed state.currentExchange directly, any future changes to currentExchange would also affect the entries in chatHistory.
+      state.chatHistory.push({ ...state.currentExchange });
+      // reset current exchange
+      state.currentExchange = { query: "", response: "" };
+    },
   },
 });
 
-export const { sendMessage } = chatSlice.actions;
+export const { sendResponse, storeQuery, addToChatHistory } = chatSlice.actions;
 export default chatSlice.reducer;
