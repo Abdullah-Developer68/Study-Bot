72860f8f0ac415b861d23f455485ce8ee7a96900
Abdullah <abdullahdeveloper843@gmail.com>
2026-01-18 20:21:27 +0500

    feat: add MarkDownEditor component and integrate MDX Editor for enhanced markdown editing fix: update tooltip arrow class for consistent styling refactor: add Tailwind CSS typography plugin to globals.css and package files

---
+++ components/editor/markdownEditor.js
@@ -0,0 +1,42 @@
+"use client";
+import {
+  MDXEditor,
+  headingsPlugin,
+  toolbarPlugin,
+  listsPlugin,
+  quotePlugin,
+  BoldItalicUnderlineToggles,
+  UndoRedo,
+  ListsToggle,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+
+// This is using MDX Editor to create a markdown editor component
+const MarkDownEditor = ({ markdown, onChange }) => {
+  return (
+    <MDXEditor
+      markdown={markdown}
+      onChange={onChange}
+      // "prose" makes it look like a real document
+      className="prose max-w-none border rounded-md shadow-sm"
+      contentEditableClassName="min-h-[300px] p-4 focus:outline-none"
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        // This plugin builds your GUI toolbar
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+              <ListsToggle />
+            </>
+          ),
+        }),
+      ]}
+    />
+  );
+};
+
+export default MarkDownEditor;
