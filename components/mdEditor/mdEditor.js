7d45df6df85122afc1d41a59bb1759684b12ba4f
Abdullah <abdullahdeveloper843@gmail.com>
2026-01-19 22:14:55 +0500

    Update mdEditor.js

--- components/mdEditor/mdEditor.js
+++ components/mdEditor/mdEditor.js
@@ -2,35 +2,55 @@
 import {
   MDXEditor,
   headingsPlugin,
-  toolbarPlugin,
   listsPlugin,
   quotePlugin,
+  toolbarPlugin,
   BoldItalicUnderlineToggles,
   UndoRedo,
   ListsToggle,
+  codeBlockPlugin,
+  linkPlugin,
+  imagePlugin,
+  tablePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  // Corrected toolbar components:
+  InsertCodeBlock, // Replaces CodeToggle
+  CreateLink, // Replaces LinkToggle
+  InsertImage, // Replaces ImageToggle
+  InsertTable, // Replaces TableToggle
 } from "@mdxeditor/editor";
 import "@mdxeditor/editor/style.css";
 
-// This is using MDX Editor package to create a markdown editor component
 const mdEditor = ({ markdown, onChange }) => {
   return (
     <MDXEditor
       markdown={markdown}
       onChange={onChange}
-      // "prose" makes it look like a real document
-      className="prose max-w-none border rounded-md shadow-sm"
-      contentEditableClassName="min-h-[300px] p-4 focus:outline-none"
+      className="prose max-w-none border rounded-md shadow-sm bg-white dark:bg-gray-800"
+      contentEditableClassName="min-h-[400px] p-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
       plugins={[
         headingsPlugin(),
         listsPlugin(),
         quotePlugin(),
-        // This plugin builds your GUI toolbar
+        codeBlockPlugin({ defaultCodeBlockLanguage: "javascript" }),
+        linkPlugin(),
+        imagePlugin(),
+        tablePlugin(),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
         toolbarPlugin({
           toolbarContents: () => (
             <>
               <UndoRedo />
               <BoldItalicUnderlineToggles />
               <ListsToggle />
+
+              {/* Corrected Toolbar Buttons */}
+              <InsertCodeBlock />
+              <CreateLink />
+              <InsertImage />
+              <InsertTable />
             </>
           ),
         }),
